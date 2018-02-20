fb25885288c9c0f7fa4a1df378e3f6e36a379b77
Yangalpha <36652031+Yangalpha@users.noreply.github.com>
2018-02-20 12:56:40 +0800

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ALBUMID="8012534"
-RSSFILE="dyzt.xml"
+RSSFILE="https://fars.ee/~\uFFFD\uFFFD\u04F0\uFFFD\uFFFD\u033D.xml"
 # export PBUUID=""
 ./ximalaya_linux_amd64 $ALBUMID > $RSSFILE
 ls
