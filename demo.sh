45eb272179946490f8bb692403d9e0d0eca6ddd9
Yangalpha <36652031+Yangalpha@users.noreply.github.com>
2018-02-20 13:06:08 +0800

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ALBUMID="8012534"
-RSSFILE="https://fars.ee/~\uFFFD\uFFFD\u04F0\uFFFD\uFFFD\u033D.xml"
+RSSFILE="8012534.xml"
 # export PBUUID=""
 ./ximalaya_linux_amd64 $ALBUMID > $RSSFILE
 ls
