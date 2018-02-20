7aaab0539fcb38d0379a8a1b828cb6f0ce845388
Yangalpha <36652031+Yangalpha@users.noreply.github.com>
2018-02-20 15:12:30 +0800

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-ALBUMID="8012534"
-RSSFILE="8012534.xml"
+ALBUMID="kdyxmly"
+RSSFILE="kdyxmly.xml"
 # export PBUUID=""
 ./ximalaya_linux_amd64 $ALBUMID > $RSSFILE
 ls
-curl -X PUT -F c=@$RSSFILE https://fars.ee/$PBUUID
+curl -X PUT -F c=@$RSSFILE https://ptpb.pw/$PBUUID
