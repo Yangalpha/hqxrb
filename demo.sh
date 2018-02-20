876b164621b7a755ad35fdd7e50c8120245e2c0b
Yangalpha <36652031+Yangalpha@users.noreply.github.com>
2018-02-20 13:36:30 +0800

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -2,7 +2,7 @@
 
 ALBUMID="8012534"
 RSSFILE="8012534.xml"
-# export PBUUID=""
+# export PBUUID="54b3a7da-b262-42dc-910a-b75cbcc1f7cd"
 ./ximalaya_linux_amd64 $ALBUMID > $RSSFILE
 ls
 curl -X PUT -F c=@$RSSFILE https://ptpb.pw/$PBUUID
